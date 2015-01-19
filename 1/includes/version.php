8967a0321bee824e2f154f55754560cdf064e27a
Remote Executive <remoteexecutive@gmail.com>
2015-01-19 10:22:49 -0600

    Vidhire Commit

---
+++ 1/includes/version.php
@@ -0,0 +1,4 @@
+<?php
+$kga['version']  = "0.9.3";             // kimai version
+$kga['revision'] = 1384;                // database revision number (incremented whenever the database changes)
+$kga['status']   = "Release Candidate"; // leave blank if stable ...
